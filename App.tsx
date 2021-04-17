cb5bf881d508cd00c886b2cf534013019a3d8af6
Enrique <sotomaque@gmail.com>
2021-04-17 16:15:42 -0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Animated from 'react-native-reanimated';
 
-export default function App() {
+export default function App(): React.ReactElement {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
